5840667896ecf90abd431bd023511a7315bc74d0
qqq <qqq@hack.more.systems>
2017-12-21 21:35:51 +0100

    top10 script tuning

--- scripts/getTop10Ctfs.py
+++ scripts/getTop10Ctfs.py
@@ -1,24 +1,39 @@
 #!/usr/bin/env python3
 
 import requests
+import argparse
+from sys import exit
 from bs4 import BeautifulSoup
+from datetime import datetime as d
 
-s = requests.session()
-resp = s.get('https://ctftime.org/team/8323')
-soup = BeautifulSoup(resp.content, 'html.parser')
-table = soup.find('table', attrs={'class':'table table-striped'})
 
-events = []
-for row in table.find_all('tr'):
-    cols = row.find_all('td')
-    if len(cols) < 5:
-        continue
-    event = {
-            'place': int(cols[1].text),
-            'name': cols[2].text,
-            'points': float(cols[4].text),
-            }
-    events.append(event)
+if __name__ == '__main__':
 
-for e in sorted(events, key=lambda k: k['points'], reverse=True)[:10]:
-    print('{:3d} {:40s} {:6.3f}'.format(e['place'], e['name'], e['points']))
+    parser = argparse.ArgumentParser(description='Process some integers.')
+    parser.add_argument('--year', default=d.now().year)
+    parser.add_argument('--team', default=8323) # LosFuzzys
+    args = parser.parse_args()
+
+    s = requests.session()
+    resp = s.get('https://ctftime.org/team/{}'.format(args.team))
+    soup = BeautifulSoup(resp.content, 'html.parser')
+    year = soup.find('div', attrs={'id': 'rating_{}'.format(args.year)})
+    if not year:
+        print("Nothing find for year {}.".format(args.year))
+        exit(-1)
+    table = year.find('table', attrs={'class':'table table-striped'})
+
+    events = []
+    for row in table.find_all('tr'):
+        cols = row.find_all('td')
+        if len(cols) < 5:
+            continue
+        event = {
+                'place': int(cols[1].text),
+                'name': cols[2].text,
+                'points': float(cols[4].text),
+                }
+        events.append(event)
+
+    for e in sorted(events, key=lambda k: k['points'], reverse=True)[:10]:
+        print('{:3d} {:40s} {:6.3f}'.format(e['place'], e['name'], e['points']))
